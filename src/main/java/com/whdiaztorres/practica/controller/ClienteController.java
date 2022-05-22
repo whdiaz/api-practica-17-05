8edf8f5bbf80a297eb019791fd5faf87e85c077d
willian honorio diaz torres <whdiaztorres@gmail.com>
2022-05-21 22:55:01 -0300

    trabalhando com jpa

--- src/main/java/com/whdiaztorres/practica/controller/ClienteController.java
+++ src/main/java/com/whdiaztorres/practica/controller/ClienteController.java
@@ -1,32 +1,23 @@
 package com.whdiaztorres.practica.controller;
-
 import com.whdiaztorres.practica.domain.model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RestController
 public class ClienteController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
 @GetMapping("/clientes")
     public List<Cliente> listar(){
-    Cliente cliente1 = new Cliente();
-    cliente1.setId(1l);
-    cliente1.setNome("gerardo3546");
-    cliente1.setEmail("ge@gmail.com");
-    cliente1.setTelefone("1234653");
-
-    Cliente cliente2 = new Cliente();
-    cliente2.setId(2l);
-    cliente2.setNome("ronaldo");
-    cliente2.setEmail("ro@gmail.com");
-    cliente2.setTelefone("234432634");
-
-     return Arrays.asList(cliente1, cliente2);
+    return  manager.createQuery("from Cliente", Cliente.class)
+            .getResultList();
 
 
 
-}
+   }
 }
--- src/main/java/com/whdiaztorres/practica/domain/model/Cliente.java
+++ src/main/java/com/whdiaztorres/practica/domain/model/Cliente.java
@@ -1,15 +1,27 @@
 package com.whdiaztorres.practica.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Cliente {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String  nome;
     private String email;
+
+    @Column(name = "fone")
     private String telefone;
+
+
 
 }
